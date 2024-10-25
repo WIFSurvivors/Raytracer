4de234b46f4aabbf4e070d72c9a2e45951585489
Spyridon Spyropoulos <spyropoulosspyros@hotmail.com>
2024-10-25 13:15:09 +0200

    Refactored code for CI integration

---
+++ core/includes/WindowEngine.h
@@ -0,0 +1,122 @@
+#pragma once
+#include "ShaderCompiler.h"
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <iostream>
+#include <utility>
+
+struct WindowEngine {
+  static void framebuffer_size_callback(GLFWwindow *window, int width,
+                                        int height);
+  static void processInput(GLFWwindow *window);
+
+  uint32_t SCR_HEIGHT = 800;
+  uint32_t SCR_WIDTH = 800;
+
+  GLFWwindow *window;
+  GLuint VBO;
+
+  void initializeEngine() {
+    if (!initGLFW())
+      throw std::runtime_error{"Failed to Initialize Engine"};
+    render_loop();
+    terminate();
+  }
+
+  void terminate() {
+    // Do Deletion Here
+    glfwTerminate();
+  }
+
+  void render_loop() {
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+                        0.0f,  0.0f,  0.5f, 0.0f};
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          reinterpret_cast<void *>(0));
+    glEnableVertexAttribArray(0);
+
+    Shader simpleShader{
+        std::make_pair(GL_VERTEX_SHADER, "../shaders/vertexshader.glsl"),
+        std::make_pair(GL_FRAGMENT_SHADER, "../shaders/fragmentshader.glsl")};
+    simpleShader.activateShader();
+
+    while (!glfwWindowShouldClose(window)) {
+      // input
+      // -----
+      processInput(window);
+
+      // render
+      // ------
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+
+      // glfw: swap buffers and poll IO events (keys pressed/released, mouse
+      // moved etc.)
+      // -------------------------------------------------------------------------------
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+    }
+  }
+
+  int initGLFW() {
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+      std::cout << "Failed to create GLFW window" << std::endl;
+      glfwTerminate();
+      return 0;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+      std::cout << "Failed to initialize GLAD" << std::endl;
+      return 0;
+    }
+    return 1;
+  }
+
+ private:
+};
+
+// process all input: query GLFW whether relevant keys are pressed/released this
+// frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+inline void WindowEngine::processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback
+// function executes
+// ---------------------------------------------------------------------------------------------
+inline void WindowEngine::framebuffer_size_callback(GLFWwindow *window,
+                                                    int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that width
+  // and height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
+}
