74a5fc98b596f83f68a4eddcc70642804d4dd4f2
Jeb44 <gabriel.zimmermann98@gmail.com>
2024-10-25 16:16:34 +0200

    made simple component a non-template for simplicity

--- core/includes/simple_component.hpp
+++ core/includes/simple_component.hpp
@@ -2,23 +2,22 @@
 
 #include "component.hpp"
 
-template<class T>
 struct simple_component : component {
   simple_component();
-  simple_component(T value, bool show_log = true);
+  simple_component(int value, bool show_log = true);
   virtual void init() override;
   virtual void update(float dt) override;
   virtual void destroy() override;
   virtual ~simple_component() override;
 
-  void set_value(T value);
-  T get_value();
+  void set_value(int value);
+  int get_value();
 
   void set_update_log(bool show_log);
   bool get_update_log();
 
 private:
-  T _value{};
+  int _value{};
   bool _show_log{true};
 
   void print_class_content();
--- core/src/simple_component.cpp
+++ core/src/simple_component.cpp
@@ -1,41 +1,39 @@
 #include "simple_component.hpp"
 
 #include <iostream>
+#include <string>
 
-template <class T> simple_component<T>::simple_component() : component{} {
+simple_component::simple_component() : component{} {
   std::cout << "-- dummy component constructed without parameter \n";
 }
 
-template <class T>
-simple_component<T>::simple_component(T value, bool show_log) : component{}, _value{value}, _show_log{show_log} {
-  std::cout << "-- dummy component constructed with parameter "
-            << print_class_content() << "\n";
+simple_component::simple_component(int value, bool show_log)
+    : component{}, _value{value}, _show_log{show_log} {
+  std::cout << "-- dummy component constructed with parameter ";
+  print_class_content();
+  std::cout << "\n";
 }
 
-template <class T> void simple_component<T>::init() {
-  std::cout << "dummy_component: init\n";
-}
+void simple_component::init() { std::cout << "dummy_component: init\n"; }
 
-template <class T> void simple_component<T>::update(float dt) {
+void simple_component::update(float dt) {
   std::cout << "dummy_component: update\n";
 }
 
-template <class T> void simple_component<T>::destroy() {
+void simple_component::destroy() { std::cout << "dummy_component: init\n"; }
+
+simple_component::~simple_component() {
   std::cout << "dummy_component: init\n";
 }
 
-template <class T> simple_component<T>::~simple_component() {
-  std::cout << "dummy_component: init\n";
-}
+void simple_component::set_update_log(bool show_log) { _show_log = show_log; }
 
-template <class T> void simple_component<T>::set_update_log(bool show_log) { _show_log = show_log; }
+bool simple_component::get_update_log() { return _show_log; }
 
-template <class T> bool simple_component<T>::get_update_log() { return _show_log; }
+void simple_component::set_value(int value) { _value = value; }
 
-template <class T> void simple_component<T>::set_value(T value) {_value = value;}
+int simple_component::get_value() { return _value; }
 
-template <class T> T simple_component<T>::get_value() { return _value; }
-
-template <class T> void simple_component<T>::print_class_content() {
-  std::cout << typeid(T).name() << ": " << _value;
+void simple_component::print_class_content() {
+  std::cout << "simple_component: " << std::to_string(_value);
 }
