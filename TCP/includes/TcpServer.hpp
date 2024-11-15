d8cc7b206f1da1f1122948d7aa374e335533225d
Spyridon Spyropoulos <spyropoulosspyros@hotmail.com>
2024-11-15 11:31:50 +0100

    Implemented multithreaded continouus Server

--- TCP/includes/TcpServer.hpp
+++ TCP/includes/TcpServer.hpp
@@ -1,12 +1,24 @@
 #pragma once
 #include <iostream>
 #include <boost/asio.hpp>
-
 #include <cstring>
+#include <thread>
+#include <chrono>
+
+using boost::asio::ip::tcp;
 
 struct TcpServer {
-  TcpServer();
-  void start(int port);
+  TcpServer(boost::asio::io_context &io_context, int port);
+  ~TcpServer();
+  void start();
   void stop();
+
 private:
+  void do_accept();
+  void handle_accept(const boost::system::error_code &error);
+  boost::asio::io_context &_io_context;
+  boost::asio::ip::tcp::acceptor _acceptor;
+  boost::asio::ip::tcp::socket _socket;
+  std::thread _server_thread;
+  bool _is_stopped;
 };
--- TCP/src/tcp_server/TcpServer.cpp
+++ TCP/src/tcp_server/TcpServer.cpp
@@ -1,87 +1,64 @@
 #include "includes/TcpServer.hpp"
 
-using boost::asio::ip::tcp;
 
-void TcpServer::start(int port) {
-    // int server_fd, new_socket;
-    // struct sockaddr_in address;
-    // int opt = 1;
-    // int addrlen = sizeof(address);
-// 
-    // // Creating socket file descriptor
-    // if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-    //     perror("socket failed");
-    //     exit(EXIT_FAILURE);
-    // }
-// 
-    // // Forcefully attaching socket to the port
-    // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-    //     perror("setsockopt");
-    //     exit(EXIT_FAILURE);
-    // }
-    // address.sin_family = AF_INET;
-    // address.sin_addr.s_addr = INADDR_ANY;
-    // address.sin_port = htons(port);
-// 
-    // // Forcefully attaching socket to the port
-    // if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    //     perror("bind failed");
-    //     exit(EXIT_FAILURE);
-    // }
-    // if (listen(server_fd, 3) < 0) {
-    //     perror("listen");
-    //     exit(EXIT_FAILURE);
-    // }
-    // if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-    //     perror("accept");
-    //     exit(EXIT_FAILURE);
-    // }
-// 
-    // std::cout << "Server started on port " << port << std::endl;
-// 
-    // // Close the socket
-    // close(new_socket);
-    // close(server_fd);#
-    try{
-        boost::asio::io_context io_context;
+TcpServer::TcpServer(boost::asio::io_context& io_context, int port)
+    : _io_context(io_context),
+      _acceptor(io_context, tcp::endpoint(tcp::v4(), port)),
+      _socket(io_context),
+      _is_stopped(false) {}
 
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 51234));
-        std::cout << "-- waiting for incoming connections...\n";
-
-        tcp::socket socket(io_context);
-        acceptor.accept(socket);
-        std::cout << "-- connection accepted\n";
-
-        char buffer[1024];
-        boost::system::error_code error;
-        size_t length = socket.read_some(boost::asio::buffer(buffer), error);
-        if(!error){
-            std::cout << "Data received: " << std::string(buffer, length) << std::endl;
-        }
-        else{
-            std::cout << "Receive failed: " << error.message() << std::endl;
-        }
-
-        socket.close();
+TcpServer::~TcpServer() {
+    if (_server_thread.joinable()) {
+        stop();
+        _server_thread.join();
     }
-    catch(std::exception& e){
-        std::cerr << "-- exception: " << e.what() << std::endl;
-    }
-
+}
+void TcpServer::start() {
+    std::cout << "-- waiting for incoming connections...\n";
+    do_accept();
+    _server_thread = std::thread([this]() { _io_context.run(); });
 }
 
-TcpServer::TcpServer()
-{
+void TcpServer::do_accept() {
+    _acceptor.async_accept(_socket, [this](const boost::system::error_code& error) {
+        handle_accept(error);
+    });
+}
 
+void TcpServer::handle_accept(const boost::system::error_code& error) {
+    if (!error && !_is_stopped) {
+        std::cout << "-- connection accepted\n";
+        char buffer[1024];
+        boost::system::error_code read_error;
+        size_t length = _socket.read_some(boost::asio::buffer(buffer), read_error);
+        if (!read_error) {
+            std::cout << "Data received: " << std::string(buffer, length) << std::endl;
+            boost::asio::write(_socket, boost::asio::buffer("Hello from server!"));
+        } else {
+            std::cout << "Receive failed: " << read_error.message() << std::endl;
+        }
+        _socket.close();
+        do_accept();
+    } else {
+        std::cout << "Accept failed: " << error.message() << std::endl;
+    }
 }
 
 void TcpServer::stop() {
+    _is_stopped = true;
+    _acceptor.close();
+    _socket.close();
     std::cout << "Server stopped" << std::endl;
 }
 
 int main() {
-    TcpServer server;
-    server.start(8080);
-    server.stop();
+    try {
+        boost::asio::io_context io_context;
+        TcpServer server(io_context, 51234);
+        server.start();
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
     return 0;
 }
