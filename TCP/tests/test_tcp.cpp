fae8744fc7818334cf8657b9355d406638e3a7d1
Spyridon Spyropoulos <spyropoulosspyros@hotmail.com>
2024-11-20 17:34:37 +0100

    Test ctest for dummy test on tcp

---
+++ TCP/tests/test_tcp.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+// Simple test case for TCP functionality
+TEST(TCPTest, DummyTest) {
+    ASSERT_EQ(1 + 1, 2);  
+}
+
+TEST(TCPTest, DummyTest2) {
+    
+    ASSERT_EQ(1 + 1, 2);  
+}
