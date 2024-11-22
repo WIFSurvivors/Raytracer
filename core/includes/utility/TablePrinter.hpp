205dc7e7fca461b3e6c7f78fd6f568ab893a7e72
Jeb44 <gabriel.zimmermann98@gmail.com>
2024-11-22 11:46:04 +0100

    fixed linker error

--- core/includes/utility/TablePrinter.hpp
+++ core/includes/utility/TablePrinter.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <iostream>
+#include <iomanip>
+
 class TablePrinter {
 public:
   //   template <typename T> static void printHeader(T t, const int &width);
-  template <typename T> static void printElement(T t, const int &width);
+  template <typename T> static void printElement(T t, const int &width) {
+    std::cout << std::left << std::setw(width) << std::setfill(_separator) << t;
+  }
 
 private:
   static const char _separator = ' ';
