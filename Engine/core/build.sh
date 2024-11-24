6d47afecb16d50078c0d33e9111b717dbe3a663a
BigWalrus <morgjik@gmail.com>
2024-11-24 20:10:24 +0100

    implemented debugging for the build script

--- Engine/core/build.sh
+++ Engine/core/build.sh
@@ -1,41 +1,29 @@
 #! /bin/bash
 
-build_ninja() {
-	mkdir -p build
-	cmake -S . -B build -G "Ninja"
-	cd build || exit
-	ninja
+build() {
+	# mkdir -p build && \
+	cmake -S . -B build -G "Unix Makefiles" && \
+	cd build || exit && \
+	make && \
 	./RayTracer
 }
 
-build_default() {
-	mkdir -p build
-	cmake -S . -B build -G "Unix Makefiles"
-	cd build || exit
-	make
-	./RayTracer
-}
-
-build_debug() {
-	mkdir -p build-debug
-	cmake -S . -B build-debug -G "Ninja" -D GLFW_BUILD_X11=1 -D GLFW_BUILD_WAYLAND=0
-	cd build-debug || exit
-	ninja
+debug() {
+	# mkdir -p build-debug && \
+	cmake -D CMAKE_BUILD_TYPE=DEBUG -D GLFW_BUILD_X11=1 -D GLFW_BUILD_WAYLAND=0 -S . -B build-debug -G "Ninja" && \
+	cd build-debug || exit && \
+	ninja && \
 	./RayTracer
 }
 
 while [[ $# -gt 0 ]];do
 	case "$1" in
-	-n | --ninja)
-	build_ninja
-		exit 1
-		;;
 	-d | --debug)
-		build_debug
+		debug
 		exit 1
 		;;
 	*)
-		build_default
+		build
 		exit 1
 		;;
 	esac
