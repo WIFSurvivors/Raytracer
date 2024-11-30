9ec6d41603087d776a20bb0536824d45322ea7d9
Alessandro <weltal01@thu.de>
2024-11-30 14:51:06 +0100

    TCP-Servercheck at application start + Draft of TreeBuilder.cs & EcsNode.cs

---
+++ GUI/RaytracerGUI/EcsNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RaytracerGUI
+{
+    class EcsNode
+    {
+        public string uuid { get; set; }
+        public string name { get; set; }
+        public int children_count { get; set; }
+        public List<EcsNode> children { get; set; }
+    }
+}
