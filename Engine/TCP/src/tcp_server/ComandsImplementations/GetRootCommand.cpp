91e7876d4a7f4779cde1c38b380a307c9a74eca9
Spyros <spyropoulosspyros@hotmail.com>
2024-12-05 21:04:35 +0100

    Created get child entities command

--- Engine/TCP/src/tcp_server/ComandsImplementations/GetRootCommand.cpp
+++ Engine/TCP/src/tcp_server/ComandsImplementations/GetRootCommand.cpp
@@ -5,6 +5,9 @@
 #include <boost/json.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/uuid.hpp>
+#include "includes/utility/JsonConverter.hpp"
+
+
 GetRootCommand::GetRootCommand() 
 {
     std::cout << "GetRootCommand::GetRootCommand()" << std::endl;
@@ -17,30 +20,7 @@ int GetRootCommand::undo()
     return 0;
 }
 
-boost::json::array entities_to_json(const std::vector<std::shared_ptr<Entity>>& entities) {
-    boost::json::array arr;
-    for (const auto& entity : entities) {
-        boost::json::object obj;
-        obj["uuid"] = boost::uuids::to_string(entity->get_uuid());
-        obj["name"] = entity->get_name();
-        obj["children_count"] = entity->get_child_entities().size();
-        arr.push_back(std::move(obj));
-    }
-    return arr;
-}
 
-boost::json::object entity_to_json(const std::shared_ptr<Entity>& entity) {
-    boost::json::object obj;
-    obj["uuid"] = boost::uuids::to_string(entity->get_uuid());
-    obj["name"] = entity->get_name();
-    obj["children_count"] = entity->get_child_entities().size();
-
-    boost::json::array children;
-    children = std::move(entities_to_json(entity->get_child_entities()));
-    obj["children"] = std::move(children);
-
-    return obj;
-}
 
 
 std::string GetRootCommand::execute(Engine * engine) 
