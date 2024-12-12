a6e7e3bbfb35dc617daeac0560fe6164b094b547
Simon <accounts+github@sschoeb.de>
2024-12-12 19:46:30 +0100

    Entity treeview working with JSON provided from ECS

---
+++ GUI/RaytracerGUI/TreeItemData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaytracerGUI
+{
+    public class TreeItemData
+    {
+        public string UUID { get; set; }
+        public string Name { get; set; }
+        public int Children { get; set; }
+    }
+}
