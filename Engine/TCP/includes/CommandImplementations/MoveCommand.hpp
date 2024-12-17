e46e9db5b4b0404e241800ffe8c1b893a454c30e
Spyros <spyropoulosspyros@hotmail.com>
2024-12-17 23:19:22 +0100

    Implemented MOVE/ROTATE/SCALE Command

--- Engine/TCP/includes/CommandImplementations/MoveCommand.hpp
+++ Engine/TCP/includes/CommandImplementations/MoveCommand.hpp
@@ -3,19 +3,18 @@
 #include <iostream>
 #include <string>
 #include <boost/uuid/uuid.hpp>
+#include <glm/vec3.hpp>
 #define MOVE_COMMAND "MoveCommand"
 class TcpCommand;
 
 class MoveCommand : public TcpCommand {
 public:
-    MoveCommand(uuid_t uuid, int new_x, int new_y, int new_z) : TcpCommand(uuid), _new_x(new_x), _new_y(new_y), _new_z(new_z) {}
+    MoveCommand(uuid_t uuid, float new_x, float new_y, float new_z) : TcpCommand(uuid), _new_position(new_x, new_y, new_z) {}
     std::string execute(Engine * engine) override;
     int undo() override;
 private:
-    int _old_x;
-    int _old_y;
-    int _old_z;
-    int _new_x;
-    int _new_y;
-    int _new_z;
+    float _old_x;
+    float _old_y;
+    float _old_z;
+    glm::vec3 _new_position;
 };
--- Engine/TCP/src/tcp_client/Gui.cs
+++ Engine/TCP/src/tcp_client/Gui.cs
@@ -12,13 +12,15 @@ public class Gui
             int port = 51234;
             EcsApi api = new EcsApi(host, port);
             // Send first message
-            //String uuid1 = api.move_entity("213123", 1.0f, 2.0f, 3.0f);
+            String uuid_place = "493fb277-5361-4e90-aa6e-f8f9a3f7c0fb"; 
+            String uuid1 = api.move_entity(uuid_place, 1.0f, 2.0f, 3.0f);
+            string uuid3 = api.scale_entity(uuid_place, 6.0f, 2.0f, 5.0f);
             // Send second message
-            //String uuid2 = api.rotate_entity("213213", 1.0f, 2.0f, 3.0f);
+            String uuid2 = api.rotate_entity(uuid_place, 3.0f, 2.0f, 1.0f);
             //client.Close();
             //string test = api.get_entity_options("c9ba5b5b-eb8e-430b-badf-e4ec7c68e9e2");
             //string message = api.get_root();
-            string message = api.get_components("35d84bb9-4289-4f10-8ca6-1c07c04b6cd4");
+            //string message = api.get_components("35d84bb9-4289-4f10-8ca6-1c07c04b6cd4");
         }
         catch (ArgumentNullException e)
         {
