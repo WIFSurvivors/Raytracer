af0c7c26784f7ba446b1df1c0c7e2acf905faba5
Jeb44 <gabriel.zimmermann98@gmail.com>
2024-12-17 10:04:13 +0100

    added exception block around Engine in main
    i hope this may catch *some* errors in time... but no guarentees!

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,6 +1,8 @@
 #include "includes/tcp_server/TcpServer.hpp"
 #include "includes/Engine.hpp"
 #include "includes/utility/SimpleLogger.hpp"
+#include <exception>
+#include <string>
 
 // #ifndef SHOW_UI
 // #define SHOW_UI
@@ -12,8 +14,13 @@ int main() {
 #else
   SimpleLogger::print("=== APP STARTED === UI DISABLED");
 #endif
-  Engine engine{};
-  engine.startLoop();
+  try {
+    Engine engine{};
+    engine.startLoop();
+  } catch (std::exception &e) {
+    SimpleLogger::print("App crashed!!!!");
+    SimpleLogger::print(std::string(e.what()));
+  }
   SimpleLogger::print("=== APP ENDED ===");
   return 0;
 }
